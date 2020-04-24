a2df2bd21522d119dfc5982fb5defb3638fe1480
Dag <dag@udelt.no>
2020-04-24 08:49:21 +0200

    Added samples for authorization details in client assertion

---
+++ HelseId.Samples.ClientCredentialsWithUnderenhet/Jwt.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using IdentityModel;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+
+namespace HelseId.Samples.ClientCredentialsWithUnderenhet
+{
+    public class Jwt
+    {
+        private static readonly int TokenLifeTimeSeconds = 60;
+
+        /// <summary>
+        /// Generates a new JWT
+        /// </summary>
+        /// <param name="clientId">The OAuth/OIDC client ID</param>
+        /// <param name="audience">The Authorization Server (STS)</param>
+        /// <param name="securityKey"></param>
+        /// <param name="extraClaims">Additional claims to add to the jwt</param>
+        /// <returns></returns>
+        public static string Generate(string clientId, string audience, SecurityKey securityKey, List<Claim> extraClaims = null)
+        {
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha512);
+
+            var payload = CreatePayload(clientId, audience, extraClaims);
+            var header = new JwtHeader(signingCredentials);
+            UpdateJwtHeader(securityKey, header);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(new JwtSecurityToken(header, payload));
+        }
+
+        private static JwtPayload CreatePayload(string clientId, string audience, List<Claim> claims = null)
+        {
+            var payload = new JwtPayload(
+               clientId,
+               audience,
+               null,
+               DateTime.UtcNow,
+               DateTime.UtcNow.AddSeconds(TokenLifeTimeSeconds));
+
+            if (claims == null)
+            {
+                return payload;
+            }
+
+            var jsonClaims = claims.Where(x => x.ValueType == "json");
+            var normalClaims = claims.Except(jsonClaims);
+
+            payload.AddClaims(normalClaims);
+
+            return AddJsonToPayload(payload, jsonClaims);
+        }
+
+        // We need to handle json objects ourself as this isn't directly supported by JwtSecurityHandler yet
+        private static JwtPayload AddJsonToPayload(JwtPayload payload, IEnumerable<Claim> jsonClaims)
+        {
+            try
+            {
+                var jsonTokens = jsonClaims.Select(x => new { x.Type, JsonValue = JToken.Parse(x.Value) }).ToArray();
+
+                var jsonObjects = jsonTokens.Where(x => x.JsonValue.Type == JTokenType.Object).ToArray();
+                var jsonObjectGroups = jsonObjects.GroupBy(x => x.Type).ToArray();
+                foreach (var group in jsonObjectGroups)
+                {
+                    if (payload.ContainsKey(group.Key))
+                    {
+                        throw new Exception(string.Format("Can't add two claims where one is a JSON object and the other is not a JSON object ({0})", group.Key));
+                    }
+
+                    if (group.Skip(1).Any())
+                    {
+                        // add as array
+                        payload.Add(group.Key, group.Select(x => x.JsonValue).ToArray());
+                    }
+                    else
+                    {
+                        // add just one
+                        payload.Add(group.Key, group.First().JsonValue);
+                    }
+                }
+
+                var jsonArrays = jsonTokens.Where(x => x.JsonValue.Type == JTokenType.Array).ToArray();
+                var jsonArrayGroups = jsonArrays.GroupBy(x => x.Type).ToArray();
+                foreach (var group in jsonArrayGroups)
+                {
+                    if (payload.ContainsKey(group.Key))
+                    {
+                        throw new Exception(string.Format("Can't add two claims where one is a JSON array and the other is not a JSON array ({0})", group.Key));
+                    }
+
+                    var newArr = new List<JToken>();
+                    foreach (var arrays in group)
+                    {
+                        var arr = (JArray)arrays.JsonValue;
+                        newArr.AddRange(arr);
+                    }
+
+                    // add just one array for the group/key/claim type
+                    payload.Add(group.Key, newArr.ToArray());
+                }
+
+                var unsupportedJsonTokens = jsonTokens.Except(jsonObjects).Except(jsonArrays);
+                var unsupportedJsonClaimTypes = unsupportedJsonTokens.Select(x => x.Type).Distinct();
+                if (unsupportedJsonClaimTypes.Any())
+                {
+                    throw new Exception(string.Format("Unsupported JSON type for claim types: {0}", unsupportedJsonClaimTypes.Aggregate((x, y) => x + ", " + y)));
+                }
+
+                return payload;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private static void UpdateJwtHeader(SecurityKey key, JwtHeader header)
+        {
+            if (key is X509SecurityKey x509Key)
+            {
+                var thumbprint = Base64Url.Encode(x509Key.Certificate.GetCertHash());
+                var x5c = GenerateX5c(x509Key.Certificate);
+                var pubKey = x509Key.PublicKey as RSA;
+                var parameters = pubKey.ExportParameters(false);
+                var exponent = Base64Url.Encode(parameters.Exponent);
+                var modulus = Base64Url.Encode(parameters.Modulus);
+
+                header.Add("x5c", x5c);
+                header.Add("kty", pubKey.SignatureAlgorithm);
+                header.Add("use", "sig");
+                header.Add("x5t", thumbprint);
+                header.Add("e", exponent);
+                header.Add("n", modulus);
+            }
+
+            if (key is RsaSecurityKey rsaKey)
+            {
+                var parameters = rsaKey.Rsa?.ExportParameters(false) ?? rsaKey.Parameters;
+                var exponent = Base64Url.Encode(parameters.Exponent);
+                var modulus = Base64Url.Encode(parameters.Modulus);
+
+                header.Add("kty", "RSA");
+                header.Add("use", "sig");
+                header.Add("e", exponent);
+                header.Add("n", modulus);
+            }
+        }
+
+        private static List<string> GenerateX5c(X509Certificate2 certificate)
+        {
+
+            var x5C = new List<string>();
+
+            var chain = GetCertificateChain(certificate);
+            if (chain != null)
+            {
+                foreach (var cert in chain.ChainElements)
+                {
+                    var x509Base64 = Convert.ToBase64String(cert.Certificate.RawData);
+                    x5C.Add(x509Base64);
+                }
+            }
+            return x5C;
+        }
+
+        private static X509Chain GetCertificateChain(X509Certificate2 cert)
+        {
+            var certificateChain = X509Chain.Create();
+            certificateChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+            certificateChain.Build(cert);
+            return certificateChain;
+        }
+    }
+}
---
+++ HelseId.Samples.ClientCredentialsWithUnderenhet/Program.cs
@@ -0,0 +1,118 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace HelseId.Samples.ClientCredentialsWithUnderenhet
+{   
+    class Program
+    {
+        private const string RsaXml = "<RSAKeyValue><Modulus>p1v3lwzMuCBKyrqjkrYS444qoovenHcaV5w0yM8NMEnUCkr3UqQ0UDkgfLt1LARUTzn2/O5VKtX6nDP63q3yO7/HMY8C9HmjJG03YmSiFRpRXL2MdDKdlL+VDwcctLEs2+5+6gWUG36uKM1XyBybumefyua1Uba4guQIS8uaJXIeFbYYzqmbMD41yio682m4Yek82hy4W5qarAA0pe3GminchUQVS4hF3vI3K7s+QBUFsneiSkyYucElWs/RgYyLVqJSGTncR48Cs20LmBHZ9CKMzzK+3YZ3sGXtJg+6ttTctUiJW3+68LDqJcSqU2P78pcAp1tD1cGUOUGG6rfc/YXUAIiDQ5r9nrscaaQBBpqNlP8PNb9b+IFLT2zEwCMp4aB0llJ4lp7JCdxUPJ4WVuRWOCeRgSqfR9l51Veq802mXrmaYyvGICBnxJJrRX06NeoIftmeRYPcOnpmRBOEhl/NXQMvCmvQPapCXHpDjuDuFF1STyBqPaPjsamrGeWXUkBhhngA+uA3FXEf3ATaaMgEED1xead+n//h0CXFwBdaBkhc31h0hl2BMdIN/Zov+8O6lNyhOwLqFCHzw0Gp2pWPEskv52Qv32/d08GgCsuYH34CyIrBp7TjJ1CnFYivMPNVoPo8/KgmkNdFs2ILHKa6iq7HrPMQ28Pmx/pCXKU=</Modulus><Exponent>AQAB</Exponent><P>0pJuMuivWzvMUROWT0lDj1H2oGF9eZSpRaTGUL8Z1q3K17PmGy/+j3WMRm81XRJr+9qidPBIXhmk7LnPmN5/nl2fLMHrfm59mWXZHL5W9ncE46BkYz0iRk4vFUbNRcPDA5OIlxbWrPc+TA1rzzCPMxo80qInur9Nq1khSd2W/zG6Etx+Kp9q6Py0ylFRkP+Fc+RU11iWnFzDiA4eC0iq67IeJF/AOoc/ir1kAIhkn/qhUczK5t6ebu2N+MyVEFKSMhzZBSver6Uku08YqaDWA5j/iMPP9Aw8L40coNwxGMtjUx5MpyFhmWFdV2m8F8eCMzw514Mty1FVUpUAqPX2gw==</P><Q>y3b2cUbeQCeHkL+U1CCgPkbxU56EQLdN/y8Izq9aLLBf6OcJsfnDANCc3OMweRD20alUmxIX41gQTYJ0K9pgM7lRTuPRcWJW89xKnCsc+Bg4+hDghbLqFFgg+/6ptwfDaCTdPD+WsYQRSJe/JjiMFbe5XU+hSa7ZbspO1k869acJOO9kB/oDZnHwEAx28BkRBR7LCP/h8h6uMbte0yiBmnl5ovfyBfyICCU642WPOra9K2wmtCdaloWkmRBIyiEYao7x0AroePnqDLTOXYd4GoavqZVq50+BrRgua5+arzAqvrCWykO2kUDP3JQujFLIo5y/qOTtRI2z737DelM3tw==</Q><DP>qh+L0K2VHwyM4eQFSEFUx/HcY27gRN4KdC3P22TJp1v5yZOakNSRwa2iizVF09ASVgQpxHhsvznQuUDVrBf22yegdjSl4hu6dbiHVGWjNLSryovHDzZQ/qQj/fiZ14d1guorLIZTIqMOPbuKInaE+zBze2lu172/LnRwJJFWcQ7n2l0xwZXSdjHUjrBsSc1nMF6E/Qahh+qaPs3JECzBinL5T0HcuGyUta6VoKiRQ37l3oSqWSP6tHxQe3Yt6GYNn1cXLspmu1mc94fL0SAUSAvQR9qLpAxOg8xqGLxNHk8UDA8qtsyNYbH8C6dtQ3j4hBRgVvGwiddIK9QeGGO/qQ==</DP><DQ>a3id+f2d/bMjl2Cqw1WsbtjYNfwADZMFXupAM7RJ5FsRfhszcs/jofWPNdnHS9ubE+nmZ7ap6YslqVtj85n4wLl9ajdJ9SMlnM/alRzsw1tAFU5+2gBERpS6b4D3slcmb0cxmNZZydBhtL961zx9Oid+gPxDzIDQFwZDmE3nbcRaSbmhU9lKnH1IeaGr3WzQIa0/P7Sxa0urZVd8Yfr+YlMR3fQr4d+fFvZbYavOeQv3Zg1NcFFtNx7Gb5c7a5EJrZdtwR9R5jzT1PxYGO0qkpBcDy5+dkn3zC9+rZhzg1/k5C6wp4wWzii24uNepv4/PrTYQ+UQMurKhZGmvWFhhw==</DQ><InverseQ>xJHmtrcMnMu5Vg0FiFWfgcivU4wIANjdNbbdaBWJU/KLnHzctWnBaB9mZlydzCWYRGmmhzwAm9X0OEb1gpT6eiSgg01/pzeIeevhBkFquw3fIRgJK2nxfiDxsSQAfjHfCQ3fEfTHSXQDd+jExbw5w681VBx+45FtU69VvJmadWQ0kCJsQVMt3e11Al07gmILlU1bjvpGeb1KZ1NiCR1O75aUgdCHyRwLwSZly2XzliO7prbnc7URnMsu+HxuQ6JybSPEWX14Pcn1BH6jJkmqflKLSKT/qiISCxD/GP+L7Jeda3fAUtW2aevFKneIk1hARa842mW/H8MQJUK6BrOTmA==</InverseQ><D>YMiiqkvQqDqkhhDhP5rj2Y0Bwva4Sivmo/vF2stCiUZoxXsNBFHJnwsqanfODyKBzz9qQmNiBV+xilvVHKnjiAIkI9jckJ03Z31xpgkkYqfRnZxQeXI8ByW0AfjO9P/xPU7zPkrzl+LuvNHjjepddLMwiZpaCWNt2OQemBaqkjUoiM3CEuGqyX9wg/VgGhxtcNH9SvWI+BC0mfuUdtDHJahHyxnQZtnr7j6NAVFLcqu1m7vrsqQRPnsgKyA7vHuWqQc+CzCW3xspKLJLHipUrQa9/6UNE/cLiIupVXWLOOhoqr3EEZIQfdkRz72n8onDzkrdKetxk1Bbc7EdYOfreBJX7Udh0cJ7bAW0gdzDinCUnu3iYOb8lnptcVlIKsXIfX1+1PoK2NOHhIX5NkhzjA0WkmYZc6LlpAFLDcC5FeNC0zIpJWlPNeFmxVqD5LhozwHz/6HcX1ohv+oD+hbF7d8tsLWOw2ROVVFSkqyN2IOC30DGrUfJxQ7si/oDeX8T9IAledZZR8UC5XmJytdLY2Ak3tw39ZaNbFlFAAQkOSKgg29Bmw5ZPzzV74qeJIUFp9Z6afsq2kLGWTOGyiuh/qQkbBv/WlCYMtKfCHPdgtOe7vISn8lrWoyfWZ5imkhCmlNqJHnawFyGD0kB3mlZtzxM6jSzOB73VRGXAoVznRE=</D></RSAKeyValue>";
+        private const string ClientId = "4944653b-7cc4-449d-bb08-12c3abb1e565";
+        private const string Scope = "udelt:test-api/api";
+        private const string TokenEndpoint = "https://localhost:44366/connect/token";
+
+        // The child organization number is provided by the EPJ
+        const string ChildOrgNo = "999977775";
+        static async Task Main(string[] args)
+        {
+
+            var clientAssertion = BuildClientAssertion(ClientId, TokenEndpoint, ChildOrgNo);
+
+            var request = new ClientCredentialsTokenRequest
+            {
+                Address = TokenEndpoint,
+                ClientId = ClientId,
+                Scope = Scope,
+                ClientAssertion = new ClientAssertion { Value = clientAssertion, Type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer" }
+            };
+
+            var result = await new HttpClient().RequestClientCredentialsTokenAsync(request);
+
+            if (result.IsError)
+            {
+                Console.Error.WriteLine("Error:");
+                Console.Error.WriteLine(result.Error);
+            }
+            else
+            {
+                Console.WriteLine("Access token:");
+                Console.WriteLine(result.AccessToken);
+                Console.WriteLine("Copy/paste the access token at https://jwt.ms to see the contents");
+            }
+        }
+
+
+        private static string BuildClientAssertion(string clientId, string audience, string childOrgNo)
+        {
+            // Builds a client assertion containing the authorization details claim with
+            // the following structure:
+            //        "authorization_details":{
+            //            "type":"helseid_authorization",
+            //            "practitioner_role":{
+            //                "organization":{
+            //                    "identifier": {
+            //                        "system":"urn:oid:2.16.578.1.12.4.1.2.101",
+            //                        "type":"ENH",
+            //                        "value":"[orgnummer]"
+            //                    }
+            //                }
+            //            }
+            //        }
+
+            var orgNumberDetails = new Dictionary<string, string>
+            {
+                { "system", "urn:oid:2.16.578.1.12.4.1.2.101" },
+                { "type", "ENH" },
+                { "value", childOrgNo }
+            };
+
+            var identifier = new Dictionary<string, object>
+            {
+                { "identifier", orgNumberDetails }
+            };
+
+            var organization = new Dictionary<string, object>
+            {
+                { "organization", identifier }
+            };
+
+            var authorizationDetails = new Dictionary<string, object>
+            {
+                { "type", "helseid_authorization" },
+                { "practitioner_role", organization }
+            };
+
+            var serialized = JsonConvert.SerializeObject(authorizationDetails);
+
+            var claims = new List<Claim>
+            {
+                new Claim("authorization_details", serialized, "json"),
+                
+                // Client assertions should have a "sub" set to ClientId
+                new Claim(JwtClaimTypes.Subject, ClientId),
+
+                // Setting a unique value for each client assertion is recommended
+                new Claim(JwtClaimTypes.JwtId, Guid.NewGuid().ToString("N"))
+            };
+
+            var rsa = new RSACng();
+            rsa.FromXmlString(RsaXml);
+            var securityKey = new RsaSecurityKey(rsa);
+
+            var assertion = Jwt.Generate(clientId,
+                audience,
+                securityKey,
+                claims);
+
+            return assertion;
+        }
+    }
+}
