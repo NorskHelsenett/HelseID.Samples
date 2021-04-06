c2a634ce917735300cbc973dd2825ddf76a8a463
Rune Andreas Grimstad <rune.andreas.grimstad@nhn.no>
2021-04-06 09:31:45 +0200

    Added sample program for jwk generation

---
+++ HelseId.RsaJwk/JsonWebKey.cs
@@ -0,0 +1,15 @@
+﻿namespace HelseId.RsaJwk
+{
+    public class JsonWebKey
+    {
+        public string D { get; set; }
+        public string DP { get; set; }
+        public string DQ { get; set; }
+        public string E { get; set; }
+        public string Kty { get; set; }
+        public string N { get; set; }
+        public string P { get; set; }
+        public string Q { get; set; }
+        public string QI { get; set; }
+    }
+}
---
+++ HelseId.RsaJwk/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HelseId.RsaJwk
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            const int keySize = 4096;
+
+            string prefix = null;
+            if (args.Length >= 1)
+            {
+                prefix = args[0];
+            }
+
+            var rsa = RSA.Create(keySize);
+            var rsaKey = new RsaSecurityKey(rsa);
+            var jwk = JsonWebKeyConverter.ConvertFromRSASecurityKey(rsaKey);
+            var privateJwk = new JsonWebKey
+            {
+                D = jwk.D,
+                DP = jwk.DP,
+                DQ = jwk.DQ,
+                E = jwk.E,
+                Kty = jwk.Kty,
+                N = jwk.N,
+                P = jwk.P,
+                Q = jwk.Q,
+                QI = jwk.QI,
+            };
+            var publicJwk = new JsonWebKey
+            {
+                Kty = jwk.Kty,
+                N = jwk.N,
+                E = jwk.E,
+            };
+
+            var serializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            {
+                WriteIndented = false,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+                IgnoreReadOnlyProperties = true,
+            };
+
+            var jwkFileName = "jwk.json";
+            var publicJwkFileName = "jwk_pub.json";
+
+            if (prefix != null)
+            {
+                jwkFileName = $"{prefix}_{jwkFileName}";
+                publicJwkFileName = $"{prefix}_{publicJwkFileName}";
+            }
+
+            File.WriteAllText(jwkFileName, JsonSerializer.Serialize(privateJwk, serializerOptions));
+            Console.WriteLine($"Wrote JWK to {jwkFileName}");
+            File.WriteAllText(publicJwkFileName, JsonSerializer.Serialize(publicJwk, serializerOptions));
+            Console.WriteLine($"Wrote public JWK to {publicJwkFileName}");
+        }
+    }
+}
