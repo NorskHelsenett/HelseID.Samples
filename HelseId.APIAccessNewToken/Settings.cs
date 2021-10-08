49187b9043b4a5bd955f01b61e906c735e04611e
Ingvild Askim Adde <ingvild.askim.adde@nhn.no>
2021-10-08 10:46:00 +0200

    Added new version of HelseId.Core.MVCHybrid.ClientAuthenticationAPIAccessNewToken.Sample.
    Changes:
    **************************************************
    - Updated framework (to .NET 5.0)
    - Changed secret type (from shared secret to jwk)
    - Changed flow to authorization code flow
    - Solution name (to HelseId.APIAccessNewToken)
    **************************************************

---
+++ HelseId.APIAccessNewToken/Settings.cs
@@ -0,0 +1,19 @@
+
+namespace HelseId.APIAccessNewToken
+{
+    public class Settings
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string Scope { get; set; }
+        public string ClientSecret { get; set; }
+        public string ResponseType { get; set; }
+        public string DefaultChallengeScheme { get; set; }
+        public string SignInScheme { get; set; }
+        public string ApiUrl { get; set; }
+        public string ApiClientId { get; set; }
+        public string ApiScope { get; set; }
+        public string RedirectUri { get; set; }
+    }
+}
+
