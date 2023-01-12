f3973ee87d064d63e920d353093e77adb8bf8650
Willy <willy.bjorneras@nhn.no>
2023-01-12 10:35:54 +0100

    Initial changes in Clien for BFF

---
+++ Blazor/Blazor.WASM.BFF/Client/BFF/AntiforgeryHandler.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+namespace Blazor.WASM.BFF.Client.BFF
+{
+    public class AntiforgeryHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("X-CSRF", "1");
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
