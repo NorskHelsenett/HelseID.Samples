6fec9d393257a819e6366b013b13728f92daaf74
Øyvind Bech <oyvind.bech@nhn.no>
2023-03-14 09:57:24 +0100

    Redirect from JavaScript

--- Node.js/ClientAuthentication/index.js
+++ Node.js/ClientAuthentication/index.js
@@ -4,7 +4,7 @@ const cookieParser        = require('cookie-parser');
 const helmet              = require('helmet');
 const passport            = require('passport');
 const http                = require('http');
-const { Issuer,Strategy } = require('openid-client');
+const { Issuer,Strategy, TokenSet } = require('openid-client');
 const https               = require("https");
 const fs                  = require('fs');
 
@@ -13,8 +13,8 @@ const fs                  = require('fs');
 // ------------------------------------------------------------
 const LOGIN_PATH = '/login';
 const LOGOUT_PATH = '/logout';
-const CALLBACK_PATH = '/login/callback';
-const LOGOUT_CALLBACK_PATH = '/logout/callback';
+const OIDC_REDIRECT_PATH = '/signin-oidc'; // '/login/callback';
+const OIDC_POST_LOGOUT_REDIRECT_PATH = '/signout-callback-oidc'; ///logout/callback';
 const LOGGED_IN_USER_PATH = '/user';
 const LOGIN_FAILURE_PATH = '/login-failure';
 const LOGIN_IN_AT_WRONG_SECURITY_LEVEL = '/login-at-wrong-security-level';
@@ -24,8 +24,8 @@ const PORT_NUMBER = 5040;
 const STS_URL = 'https://helseid-sts.test.nhn.no';
 const CLIENT_ID = 'helseid-sample-client-authentication';
 const CLIENT_SCOPE = 'openid profile helseid://scopes/identity/pid helseid://scopes/identity/security_level';
-const REDIRECT_URI = `https://localhost:${PORT_NUMBER}${CALLBACK_PATH}`
-const POST_LOGOUT_REDIRECT_URI = `https://localhost:${PORT_NUMBER}${LOGOUT_CALLBACK_PATH}`
+const OIDC_REDIRECT_URI = `https://localhost:${PORT_NUMBER}${OIDC_REDIRECT_PATH}`
+const OIDC_POST_LOGOUT_REDIRECT_URI = `https://localhost:${PORT_NUMBER}${OIDC_POST_LOGOUT_REDIRECT_PATH}`
 const PRIVATE_KEY_RSA = {
   'd': 'dJFf2efy5al1PZPEtgbifR0E7IC1yjTZn8OcgjdoVsaJG7F7rrAUH4awxyXJaOrHfz0ZwNtp57TzKtO50Qy08PPcg8uHO4WIpvmHgS_mtNSovWVxsWVnGdq0NlgAT8EEhVT1-3acu6OvfOROM8HfWgzded8dkWPm9QMPJtd9q2u0-ZGdEDu71ttHfFHHb8BZK5C6AX-BBaEpJQ8fsmXyO8MLmXGos4Q95XX40qgsMqMrxLIYNCjcmXkdOq9gfHd1d5ub-X84u2MzVc3qXDr3F5YgCmx0i4sKddFrQ4J28rIjTWnyYPJDOzpSVCBuazVLkSY7I15CwCZnMU_1BdkEWowzULWNQVwB2vLQTgbS2uMGci2R0QbmxCqMt2teQ9W4sroHcantTJjEcPx0K4wpy0BGhHGJXcecFw78QD5qUzUyM5TyakyEIfbVicZHDPcbQyx4xxQlhNugrljD4UelRE2_In53Re1kNPBUwiC_Ql0fMlilxyU6R1lLPUoz-BnATZn9ofJB1UQqUKYUxPB763I16WLvQwIP42unKww4HPEKTPojWzwy7igFQklJ0IavdXpQtebTQeXLuyhsikX1wYe1tX6zSFRcCDUukfz3YQb-BTGpWLFcIjbqvYND08sYDGwXcOfQeHwJIy-4MHti4HpYgy5QvibrQHfGH7soxxU',
   'dp': '1NS8GseB2YzZfkOYe9aFcvqrFTbdvShYDhbd2we4ukViZaLUQ1FnfzgTzh8WDi_7Py1rwsZZHfpZjSyI81_VGP919_kcDtf74IK502nLpztoaLRgELGZbdDCadAMb4nXGMRfoFG5esl3vRQU2JQuOnglFNccBuq61d7VPe5F8J2eF-NmtxrcxZeSvSHBGEbDFCE9xz-yCwS0sbM-ymcSVP937frfXdYztaASgUL1Mvn7EoyHcXXV787uOlpJ306lp9bTOVzeRpRy_2apQoLucq5pCSKyame7oeinpVrvbfkvdTXHZkZnS1uNv3D8cKiyt8GXKG0nUw-7SYX_Rg-6aQ',
@@ -45,6 +45,10 @@ const SIGNING_ALGORITHM_RSA = 'RS384';
 // ------------------------------------------------------------
 // https://codeburst.io/how-to-implement-openid-authentication-with-openid-client-and-passport-in-node-js-43d020121e87
 // https://www.codespeedy.com/how-to-enable-https-in-express-js/
+// To create new certificates:
+// openssl req -x509 -nodes -days 8000 -newkey rsa:2048 -keyout ./sslcert/server.key -out ./sslcert/server.crt
+// https://stackoverflow.com/questions/11744975/enabling-https-on-express-js
+// https://github.com/goranlisak/openid-connect-sample
 const app = express();
 
 app.use(cookieParser());
@@ -63,7 +67,8 @@ app.use(passport.session());
 
 // Gets a serialized user object into the session
 passport.serializeUser((user, done) => {
-  /*
+  // Uncomment to debug result:
+  /*  
     console.log('-----------------------------');
     console.log('serialize user');
     console.log(user);
@@ -74,6 +79,7 @@ passport.serializeUser((user, done) => {
 
 // Deserializes user objects out of the session
 passport.deserializeUser((user, done) => {
+  // Uncomment to debug result:
   /*
     console.log('-----------------------------');
     console.log('deserialize user');
@@ -91,11 +97,11 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
 
   var client = new oidcIssuer.Client({
     client_id: CLIENT_ID,
-    redirect_uris: [ REDIRECT_URI ],
+    redirect_uris: [ OIDC_REDIRECT_URI ],
     response_types: [ 'code' ],    
     token_endpoint_auth_signing_alg: SIGNING_ALGORITHM_RSA,
     token_endpoint_auth_method: 'private_key_jwt',
-    post_logout_redirect_uris: [ POST_LOGOUT_REDIRECT_URI ],
+    post_logout_redirect_uris: [ OIDC_POST_LOGOUT_REDIRECT_URI ],
   },
   { 
     // This is needed to sign the client assertion:
@@ -105,18 +111,31 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
   passport.use(
     PASSPORT_OIDC_STRATEGY,
     new Strategy({ client, passReqToCallback: true }, (req, tokenSet, userinfo, done) => {
-      //console.log('tokenSet', tokenSet);
-      //console.log('userinfo', userinfo);
+      // Uncomment to debug result:
+    /*  
+      console.log('-----------------------------');
+      console.log('tokenSet', tokenSet);
+      console.log('userinfo', userinfo);
+      console.log('-----------------------------');
+      */
       req.session.tokenSet = tokenSet;
       req.session.userinfo = userinfo;
-      return done(null, tokenSet.claims());
+      const user = tokenSet.claims();
+      user['id_token'] = tokenSet['id_token'];
+      return done(null, user);
     }));
-
-
+    /*
+    passport.use('oidc', new Strategy({ client, params }, (tokenset, userinfo, done) => {
+      return done(null, userinfo); // return user information
+  }));
+*/
   // ------------------------------------------------------------
   // Application paths
   // ------------------------------------------------------------
 
+  // ------------------------------------------------------------
+  // Index page
+  // ------------------------------------------------------------
   app.get('/', (req, res) => {
     res.send(`
         <h1>Welcome</h1>
@@ -125,6 +144,16 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
         `);
   });
 
+  // ------------------------------------------------------------
+  // The user was redirected after logout
+  // ------------------------------------------------------------
+  app.get(OIDC_POST_LOGOUT_REDIRECT_PATH, (req, res) => {
+    return res.redirect('/');
+  });
+
+  // ------------------------------------------------------------
+  // The user was logged on at a wrong security level
+  // ------------------------------------------------------------
   app.get(LOGIN_IN_AT_WRONG_SECURITY_LEVEL, (req, res) => {
     res.send(`
         <h3>Access denied</h3>
@@ -133,24 +162,33 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
         `);
   });
 
+  // ------------------------------------------------------------
+  // Starting the passport authenticate function
+  // ------------------------------------------------------------
   app.get(LOGIN_PATH, (req, res, next)  => {
-    console.log('Starting the passport authenticate function');
     next();
   }, passport.authenticate(PASSPORT_OIDC_STRATEGY, {scope: CLIENT_SCOPE}));
 
-  app.get(CALLBACK_PATH, (req, res, next) => {
+  // ------------------------------------------------------------
+  // The user has been redirected from HelseID:
+  // ------------------------------------------------------------
+  app.get(OIDC_REDIRECT_PATH, (req, res, next) => {
     passport.authenticate(PASSPORT_OIDC_STRATEGY, { successRedirect: LOGGED_IN_USER_PATH, failureRedirect: LOGIN_FAILURE_PATH })
     (req, res, next)
   });
 
+  // ------------------------------------------------------------
+  // The user has been authenticated:
+  // ------------------------------------------------------------
   app.get(LOGGED_IN_USER_PATH, (req, res) => {
 
     if (req.session.passport == undefined || req.session.passport.user == undefined) {
       return res.redirect('/');
     }
 
-    var user = req.session.passport.user;
-    
+    const user = req.session.passport.user;
+    // the user info is stored as a TokenSet:
+
     if (user['helseid://claims/identity/security_level'] !== '4') {
       // The security level is not at the required value
       return res.redirect(LOGIN_IN_AT_WRONG_SECURITY_LEVEL);
@@ -168,18 +206,29 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
         `);  
   });
 
+  // ------------------------------------------------------------
+  // Logs out the user from HelseID:
+  // ------------------------------------------------------------
   app.get(LOGOUT_PATH, (req, res) => {
-    res.redirect(client.endSessionUrl());
+    //const ession = req.sessionStore.get(req.session.id);
+    // We need the ID token in order to indicate to HelseID what our session is
+    const user = req.session.passport.user;
+    res.redirect(client.endSessionUrl({id_token_hint: user['id_token'] }));
   });
 
-  // logout callback
-  app.get(LOGOUT_CALLBACK_PATH, (req, res) => {
+  // ------------------------------------------------------------
+  // This gets redirected from HelseID post logout:
+  // ------------------------------------------------------------
+  app.get(OIDC_POST_LOGOUT_REDIRECT_URI, (req, res) => {
     // clears the persisted user from the local storage
     req.logout();
     // redirects the user to a public route
     res.redirect('/');
   });
 
+  // ------------------------------------------------------------
+  // Login error:
+  // ------------------------------------------------------------
   app.get(LOGIN_FAILURE_PATH, (req, res) => {
     res.send(`
         <h1>Error</h1>
@@ -193,22 +242,14 @@ Issuer.discover(STS_URL).then(oidcIssuer => {
 // ------------------------------------------------------------
 // Start the application
 // ------------------------------------------------------------
-// Create a NodeJS HTTPS listener on port 4000 that points to the Express app
+// Create a NodeJS HTTPS listener on port 5040 that points to the Express app
 // Use a callback function to tell when the server is created.
-// openssl req -x509 -nodes -days 8000 -newkey rsa:2048 -keyout ./sslcert/server.key -out ./sslcert/server.crt
-// https://stackoverflow.com/questions/11744975/enabling-https-on-express-js
-var privateKey = fs.readFileSync('sslcert/server.key').toString();
-var certificate = fs.readFileSync('sslcert/server.crt').toString();
-
-var credentials = {key: privateKey, cert: certificate};
+const privateKey = fs.readFileSync('sslcert/server.key').toString();
+const certificate = fs.readFileSync('sslcert/server.crt').toString();
+const credentials = {key: privateKey, cert: certificate};
 
 https
   .createServer(credentials, app)
   .listen(PORT_NUMBER, ()=> {
     console.log(`Now listening on: https://localhost:${PORT_NUMBER}`)
   });
-/*
-  http.createServer(app).listen(PORT_NUMBER, () => {
-  console.log(`Now listening on: http://localhost:${PORT_NUMBER}`)
-});
-*/
