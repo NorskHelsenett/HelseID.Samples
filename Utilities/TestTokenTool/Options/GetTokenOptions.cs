9c5a8a9da38f12a98ba92af58a67b0d10bb0d2ca
Øyvind Bech <oyvind.bech@nhn.no>
2023-06-22 17:40:14 +0200

    Options tweaking

---
+++ Utilities/TestTokenTool/Options/GetTokenOptions.cs
@@ -0,0 +1,119 @@
+using CommandDotNet;
+using TestTokenTool.Constants;
+using TestTokenTool.InputParameters;
+
+// ReSharper disable InconsistentNaming
+
+namespace TestTokenTool.Options;
+
+public class GetTokenOptions : IArgumentModel
+{
+    // -----------------------------------------
+    // Output from application
+    [Option('p', Description = "The returned token is printed in JWT format on screen", BooleanMode = BooleanMode.Explicit)]
+    public bool printToken { get; set; } = true;
+    [Option(Description = "The returned token is printed in JSON format on screen")]
+    public bool prettyPrintToken { get; set; } = false;
+    [Option('s', Description = $"The returned token is saved to a file named '{FileConstants.TokenFileName}'")]
+    public bool saveTokenToFile { get; set; } = false;
+    // -----------------------------------------
+    // Invalid output from TTT
+    [Option(Description = "The returned token will be signed with an invalid signing key")]
+    public bool signJwtWithInvalidSigningKey { get; set; } = false;
+    [Option(Description = "The returned token will contain an invalid 'iss' claim")]
+    public bool setInvalidIssuer { get; set; } = false;
+    [Option(Description = "The returned token will contain an invalid 'aud' claim")]
+    public bool setInvalidAudience { get; set; } = false;
+    [Option(Description = "The returned token will contain an expired 'nbf', 'iat', and 'exp' claims")]
+    public bool setExpirationTimeAsExpired { get; set; } = false;
+    [Option(Description = "The returned token will contain an 'exp' claim matching the set expiration time")]
+    public int expirationTimeInSeconds { get; set; } = 600;
+    // -----------------------------------------
+    // Header parameters
+    [Option(Description = "The returned token will contain a 'typ' header matching the injected value. Accepted values are 'jwt' and 'at+jwt'")]
+    public string? typHeader { get; set; } = string.Empty;
+    // -----------------------------------------
+    // General parameters
+    [Option(Description = "The returned token will contain a 'client_amr' claim matching the injected value")]
+    public string? clientAmr { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'helseid://claims/client/amr' claim matching the injected value")]
+    public string? helseidClientAmr { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'helseid://claims/client/claims/orgnr_parent' claim matching the injected value")]
+    public string? orgnrParent { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'client_id' claim matching the injected value")]
+    public Guid? ClientId { get; set;} = default;
+    [Option(Description = "The returned token will contain a 'helseid://claims/client/client_name' claim matching the injected value")]
+    public string? clientName { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'scope' claim matching the injected value. Use quotes and spaces to insert several scopes.")]
+    public string? scope { get; set; } = string.Empty;
+    // -----------------------------------------
+    // User parameters
+    [Option(Description = "The returned token will contain an 'amr' claim matching the injected value")]
+    public string? amr { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain an 'idp' claim matching the injected value")]
+    public string? identityProvider { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'helseid://claims/identity/assurance_level' claim matching the injected value")]
+    public string? assuranceLevel { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'helseid://claims/identity/security_level' claim matching the injected value")]
+    public string? securityLevel { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a claim with a pseudonymized pid value. Requires that the 'pid' parameter is set.")]
+    public bool setPidPseudonym { get; set; } = false;
+    [Option(Description = "The returned token will contain a 'pid' claim matching the injected value")]
+    public string? pid { get; set; } = string.Empty;
+    [Option(Description = "If this value is set, the PID value will be used to extract person information from Persontjenesten. Requires that the 'pid' parameter is set.")]
+    public bool getPersonFromPersontjenesten { get; set; } = false;
+    [Option(Description = "The returned token will contain a 'helseid://claims/identity/pid_pseudonym' claim matching the injected value. If setPidPseudonym is true, this option will be overridden.")]
+    public string? pidPseudonym { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'name' claim matching the injected value. Requires that the 'getPersonFromPersontjenesten' parameter is not set.")]
+    public string? name { get; set; } = string.Empty;
+    [Option(Description = "If this value is set, the PID value will be used to extract person information from HPR-registeret.")]
+    public bool getHprNumberFromHprregisteret { get; set; } = false;
+    [Option(Description = "The returned token will contain a 'helseid://claims/hpr/hpr_number' claim matching the injected value. Requires that the 'getHprNumberFromHprregisteret' parameter is not set.")]
+    public string? hprNumber { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'network' claim matching the injected value")]
+    public string? network { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'sid' claim matching the injected value")]
+    public string? sid { get; set; } = string.Empty;
+    [Option(Description = "The returned token will contain a 'sub' claim matching the injected value")]
+    public string? sub { get; set; } = string.Empty;
+    // -----------------------------------------
+    // Tillitsrammeverk parameters
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string legalEntityId { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string legalEntityName { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string pointOfCareId { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string pointOfCareName { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string practitionerAuthorizationCode { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
+    public string practitionerAuthorizationText { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipDepartmentId { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipDepartmentName { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipHealthcareServiceCode { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipHealthcareServiceText { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipPurposeOfUseCode { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipPurposeOfUseText { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipPurposeOfUseDetailsCode { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipPurposeOfUseDetailsText { get; set; } = string.Empty;
+    [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
+    public string careRelationshipTracingRefId { get; set; } = string.Empty;
+    // -----------------------------------------
+    // Usage of parameters
+    [Option(Description = "Instructs how common claims are created")]
+    public ClaimGeneration generalClaimsCreation { get; set; } = ClaimGeneration.DefaultWithParameterValues;
+    [Option(Description = "Instructs how user claims are created")]
+    public ClaimGeneration userClaimsCreation { get; set; } = ClaimGeneration.DefaultWithParameterValues;
+    [Option(Description = "Create claims for dokumentdeling")]
+    public bool createDokumentdelingClaims { get; set; } = false;
+}
--- Utilities/TestTokenTool/Program.cs
+++ Utilities/TestTokenTool/Program.cs
@@ -1,10 +1,12 @@
 ﻿using CommandDotNet;
+using CommandDotNet.Help;
 using Microsoft.Extensions.Configuration;
 using TestTokenTool.ApiCalls;
 using TestTokenTool.Commands;
 using TestTokenTool.Configuration;
 using TestTokenTool.Constants;
 using TestTokenTool.InputParameters;
+using TestTokenTool.Options;
 using TestTokenTool.RequestModel;
 using TokenRequest = TestTokenTool.RequestModel.TokenRequest;
 
@@ -20,120 +22,29 @@ public class Program
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("config.json", optional: false);
 
-        var appRunner = new AppRunner<Program>();
+        var appSettings = new AppSettings
+        {
+            Parser = new ParseAppSettings
+            {
+                AllowSingleHyphenForLongNames = true,
+            },
+            Arguments = new ArgumentAppSettings
+            {
+                BooleanMode = BooleanMode.Implicit,
+            },
+            Help = new AppHelpSettings
+            {
+                TextStyle = HelpTextStyle.Basic,
+                UsageAppName = "dotnet run",
+            }
+        };
+        var appRunner = new AppRunner<Program>(appSettings);
+        appRunner.UseTypoSuggestions();
         await appRunner.RunAsync(args);
     }
 
     [Command("getToken", Description = "Get a token from the test token service")] 
-    public async Task GetToken(
-        // -----------------------------------------
-        // Output from application
-        [Option('p', Description = "The returned token is printed in JWT format on screen", BooleanMode = BooleanMode.Explicit)]
-        bool printToken = true,
-        [Option(Description = "The returned token is printed in JSON format on screen", BooleanMode = BooleanMode.Implicit)]
-        bool prettyPrintToken = false,
-        [Option('s', Description = $"The returned token is saved to a file named '{FileConstants.TokenFileName}'", BooleanMode = BooleanMode.Implicit)]
-        bool saveTokenToFile = false,
-        // -----------------------------------------
-        // Invalid output from TTT
-        [Option(Description = "The returned token will be signed with an invalid signing key", BooleanMode = BooleanMode.Implicit)]
-        bool signJwtWithInvalidSigningKey = false,
-        [Option(Description = "The returned token will contain an invalid 'iss' claim", BooleanMode = BooleanMode.Implicit)]
-        bool setInvalidIssuer = false,
-        [Option(Description = "The returned token will contain an invalid 'aud' claim", BooleanMode = BooleanMode.Implicit)]
-        bool setInvalidAudience = false,
-        [Option(Description = "The returned token will contain an expired 'nbf', 'iat', and 'exp' claims", BooleanMode = BooleanMode.Implicit)]
-        bool setExpirationTimeAsExpired = false,
-        [Option(Description = "The returned token will contain an 'exp' claim matching the set expiration time")]
-        int expirationTimeInSeconds = 600,
-        // -----------------------------------------
-        // Header parameters
-        [Option(Description = "The returned token will contain a 'typ' header matching the injected value. Accepted values are 'jwt' and 'at+jwt'")]
-        string? typ = "",
-        // -----------------------------------------
-        // General parameters
-        [Option(Description = "The returned token will contain a 'client_amr' claim matching the injected value")]
-        string? clientAmr = "",
-        [Option(Description = "The returned token will contain a 'helseid://claims/client/amr' claim matching the injected value")]
-        string? helseidClientAmr = "",
-        [Option(Description = "The returned token will contain a 'helseid://claims/client/claims/orgnr_parent' claim matching the injected value")]
-        string? orgnrParent = "",
-        [Option(Description = "The returned token will contain a 'client_id' claim matching the injected value")]
-        Guid? clientId = default,
-        [Option(Description = "The returned token will contain a 'helseid://claims/client/client_name' claim matching the injected value")]
-        string? clientName = "",
-        [Option(Description = "The returned token will contain a 'scope' claim matching the injected value. Use quotes and spaces to insert several scopes.")]
-        string? scope = "",
-        // -----------------------------------------
-        // User parameters
-        [Option(Description = "The returned token will contain an 'amr' claim matching the injected value")]
-        string? amr = "",
-        [Option(Description = "The returned token will contain an 'idp' claim matching the injected value")]
-        string? identityProvider = "",
-        [Option(Description = "The returned token will contain a 'helseid://claims/identity/assurance_level' claim matching the injected value")]
-        string? assuranceLevel = "",
-        [Option(Description = "The returned token will contain a 'helseid://claims/identity/security_level' claim matching the injected value")]
-        string? securityLevel = "",
-        [Option(Description = "The returned token will contain a claim with a pseudonymized pid value. Requires that the 'pid' parameter is set.", BooleanMode = BooleanMode.Implicit)]
-        bool setPidPseudonym = false,
-        [Option(Description = "The returned token will contain a 'pid' claim matching the injected value")]
-        string? pid = "",
-        [Option(Description = "If this value is set, the PID value will be used to extract person information from Persontjenesten. Requires that the 'pid' parameter is set.", BooleanMode = BooleanMode.Implicit)]
-        bool getPersonFromPersontjenesten = false,
-        [Option(Description = "The returned token will contain a 'helseid://claims/identity/pid_pseudonym' claim matching the injected value. If setPidPseudonym is true, this option will be overridden.")]
-        string? pidPseudonym = "",
-        [Option(Description = "The returned token will contain a 'name' claim matching the injected value. Requires that the 'getPersonFromPersontjenesten' parameter is not set.")]
-        string? name = "",
-        [Option(Description = "If this value is set, the PID value will be used to extract person information from HPR-registeret.", BooleanMode = BooleanMode.Implicit)]
-        bool getHprNumberFromHprregisteret = false,
-        [Option(Description = "The returned token will contain a 'helseid://claims/hpr/hpr_number' claim matching the injected value. Requires that the 'getHprNumberFromHprregisteret' parameter is not set.")]
-        string? hprNumber = "",
-        [Option(Description = "The returned token will contain a 'network' claim matching the injected value")]
-        string? network = "",
-        [Option(Description = "The returned token will contain a 'sid' claim matching the injected value")]
-        string? sid = "",
-        [Option(Description = "The returned token will contain a 'sub' claim matching the injected value")]
-        string? sub = "",
-        // -----------------------------------------
-        // Tillitsrammeverk parameters
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string legalEntityId = "",
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string legalEntityName = "",
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string pointOfCareId = "",
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string pointOfCareName = "",
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string practitionerAuthorizationCode = "",
-        [Option(Description = "Parameter for use in 'tillitsrammeverk' claims")]
-        string practitionerAuthorizationText = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipDepartmentId = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipDepartmentName = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipHealthcareServiceCode = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipHealthcareServiceText = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipPurposeOfUseCode = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipPurposeOfUseText = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipPurposeOfUseDetailsCode = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipPurposeOfUseDetailsText = "",
-        [Option(Description = "Parameter for use in 'dokumentdeling' claims")]
-        string careRelationshipTracingRefId = "",
-        // -----------------------------------------
-        // Usage of parameters
-        [Option(Description = "Instructs how common claims are created")]
-        ClaimGeneration generalClaimsCreation = ClaimGeneration.DefaultWithParameterValues,
-        [Option(Description = "Instructs how user claims are created")]
-        ClaimGeneration userClaimsCreation = ClaimGeneration.DefaultWithParameterValues,
-        [Option(Description = "Create claims for dokumentdeling", BooleanMode = BooleanMode.Implicit)]
-        bool createDokumentdelingClaims = false)
+    public async Task GetToken(GetTokenOptions getTokenOptions)
     {
         
         if (!File.Exists(FileConstants.JwkFileName))
@@ -142,79 +53,76 @@ public class Program
             return;
         }
         
-        // På tjener: hvis getHprNumberFraRegister er satt, ikke skriv hprnummer først
-        
         var tokenRequest = new TokenRequest
         {
-            SignJwtWithInvalidSigningKey = signJwtWithInvalidSigningKey,
-            SetInvalidIssuer = setInvalidIssuer,
-            SetInvalidAudience = setInvalidAudience,
-            SetExpirationTimeAsExpired = setExpirationTimeAsExpired,
-            ExpirationTimeInSeconds = expirationTimeInSeconds,
-            SetPidPseudonym = setPidPseudonym,
-            GeneralClaimsParametersGeneration = generalClaimsCreation.ToParametersGeneration(),
-            UserClaimsParametersGeneration = userClaimsCreation.ToParametersGeneration(),
-            CreateDokumentdelingClaims = createDokumentdelingClaims,
+            SignJwtWithInvalidSigningKey = getTokenOptions.signJwtWithInvalidSigningKey,
+            SetInvalidIssuer = getTokenOptions.setInvalidIssuer,
+            SetInvalidAudience = getTokenOptions.setInvalidAudience,
+            SetExpirationTimeAsExpired = getTokenOptions.setExpirationTimeAsExpired,
+            ExpirationTimeInSeconds = getTokenOptions.expirationTimeInSeconds,
+            SetPidPseudonym = getTokenOptions.setPidPseudonym,
+            GeneralClaimsParametersGeneration = getTokenOptions.generalClaimsCreation.ToParametersGeneration(),
+            UserClaimsParametersGeneration = getTokenOptions.userClaimsCreation.ToParametersGeneration(),
+            CreateDokumentdelingClaims = getTokenOptions.createDokumentdelingClaims,
             HeaderParameters = new HeaderParameters()
             {
-                Typ = typ.GetEmptyStringIfNotSet(), 
+                Typ = getTokenOptions.typHeader.GetEmptyStringIfNotSet(), 
             },
             GeneralClaimsParameters = new GeneralParameters
             {
-                AuthenticationMethodsReferences = clientAmr.GetListWithParameter(),
-                ClientAuthenticationMethodsReferences = helseidClientAmr.GetEmptyStringIfNotSet(),
-                OrgnrParent = orgnrParent.GetEmptyStringIfNotSet(),
-                ClientId = clientId != null ? clientId.ToString()! : string.Empty,
-                ClientName = clientName.GetEmptyStringIfNotSet(),
-                Scope = scope.GetListWithMultipleParameters(),
+                AuthenticationMethodsReferences = getTokenOptions.clientAmr.GetListWithParameter(),
+                ClientAuthenticationMethodsReferences = getTokenOptions.helseidClientAmr.GetEmptyStringIfNotSet(),
+                OrgnrParent = getTokenOptions.orgnrParent.GetEmptyStringIfNotSet(),
+                ClientId = getTokenOptions.ClientId != null ? getTokenOptions.ClientId.ToString()! : string.Empty,
+                ClientName = getTokenOptions.clientName.GetEmptyStringIfNotSet(),
+                Scope = getTokenOptions.scope.GetListWithMultipleParameters(),
             },
             UserClaimsParameters = new UserClaimsParameters
             {
-                Pid = pid.GetEmptyStringIfNotSet(),
-                Network = network.GetEmptyStringIfNotSet(),
-                // Service: set error when this is set for GetPersonFromPersontjenesten
-                Name = name.GetEmptyStringIfNotSet(),
-                Sid = sid.GetEmptyStringIfNotSet(),
-                Subject = sub.GetEmptyStringIfNotSet(),
-                AssuranceLevel = assuranceLevel.GetEmptyStringIfNotSet(),
-                SecurityLevel = securityLevel.GetEmptyStringIfNotSet(),
-                HprNumber = hprNumber.GetEmptyStringIfNotSet(),
-                IdentityProvider = identityProvider.GetEmptyStringIfNotSet(),
-                PidPseudonym = pidPseudonym.GetEmptyStringIfNotSet(),
-                Amr = amr.GetEmptyStringIfNotSet(),
+                Pid = getTokenOptions.pid.GetEmptyStringIfNotSet(),
+                Network = getTokenOptions.network.GetEmptyStringIfNotSet(),
+                Name = getTokenOptions.name.GetEmptyStringIfNotSet(),
+                Sid = getTokenOptions.sid.GetEmptyStringIfNotSet(),
+                Subject = getTokenOptions.sub.GetEmptyStringIfNotSet(),
+                AssuranceLevel = getTokenOptions.assuranceLevel.GetEmptyStringIfNotSet(),
+                SecurityLevel = getTokenOptions.securityLevel.GetEmptyStringIfNotSet(),
+                HprNumber = getTokenOptions.hprNumber.GetEmptyStringIfNotSet(),
+                IdentityProvider = getTokenOptions.identityProvider.GetEmptyStringIfNotSet(),
+                PidPseudonym = getTokenOptions.pidPseudonym.GetEmptyStringIfNotSet(),
+                Amr = getTokenOptions.amr.GetEmptyStringIfNotSet(),
             },
             TillitsrammeverkClaimsParameters = new TillitsrammeverkClaimsParameters
             {
-                PractitionerAuthorizationCode = practitionerAuthorizationCode,
-                PractitionerAuthorizationText = practitionerAuthorizationText, 
-                LegalEntityId = legalEntityId,
-                LegalEntityName = legalEntityName,
-                PointOfCareId = pointOfCareId,
-                PointOfCareName = pointOfCareName,
+                PractitionerAuthorizationCode = getTokenOptions.practitionerAuthorizationCode,
+                PractitionerAuthorizationText = getTokenOptions.practitionerAuthorizationText, 
+                LegalEntityId = getTokenOptions.legalEntityId,
+                LegalEntityName = getTokenOptions.legalEntityName,
+                PointOfCareId = getTokenOptions.pointOfCareId,
+                PointOfCareName = getTokenOptions.pointOfCareName,
             },
             DokumentdelingClaimsParameters = new DokumentdelingClaimsParameters
             {
-              CareRelationshipDepartmentId  = careRelationshipDepartmentId,
-              CareRelationshipDepartmentName = careRelationshipDepartmentName,
-              CareRelationshipHealthcareServiceCode = careRelationshipHealthcareServiceCode,
-              CareRelationshipHealthcareServiceText = careRelationshipHealthcareServiceText,
-              CareRelationshipPurposeOfUseCode = careRelationshipPurposeOfUseCode,
-              CareRelationshipPurposeOfUseText = careRelationshipPurposeOfUseText,
-              CareRelationshipPurposeOfUseDetailsCode = careRelationshipPurposeOfUseDetailsCode,
-              CareRelationshipPurposeOfUseDetailsText = careRelationshipPurposeOfUseDetailsText, 
-              CareRelationshipTracingRefId = careRelationshipTracingRefId,
+              CareRelationshipDepartmentId  = getTokenOptions.careRelationshipDepartmentId,
+              CareRelationshipDepartmentName = getTokenOptions.careRelationshipDepartmentName,
+              CareRelationshipHealthcareServiceCode = getTokenOptions.careRelationshipHealthcareServiceCode,
+              CareRelationshipHealthcareServiceText = getTokenOptions.careRelationshipHealthcareServiceText,
+              CareRelationshipPurposeOfUseCode = getTokenOptions.careRelationshipPurposeOfUseCode,
+              CareRelationshipPurposeOfUseText = getTokenOptions.careRelationshipPurposeOfUseText,
+              CareRelationshipPurposeOfUseDetailsCode = getTokenOptions.careRelationshipPurposeOfUseDetailsCode,
+              CareRelationshipPurposeOfUseDetailsText = getTokenOptions.careRelationshipPurposeOfUseDetailsText, 
+              CareRelationshipTracingRefId = getTokenOptions.careRelationshipTracingRefId,
             },
-            GetPersonFromPersontjenesten = getPersonFromPersontjenesten,
-            GetHprNumberFromHprregisteret = getHprNumberFromHprregisteret,
+            GetPersonFromPersontjenesten = getTokenOptions.getPersonFromPersontjenesten,
+            GetHprNumberFromHprregisteret = getTokenOptions.getHprNumberFromHprregisteret,
         };
 
         var tokenResponse = await TokenRetriever.GetToken(_builder!, tokenRequest);
 
         var parameters = new Parameters()
         {
-            PrintJwt = printToken,
-            PrettyPrintJwt = prettyPrintToken,
-            SaveTokenToFile = saveTokenToFile,
+            PrintJwt = getTokenOptions.printToken,
+            PrettyPrintJwt = getTokenOptions.prettyPrintToken,
+            SaveTokenToFile = getTokenOptions.saveTokenToFile,
         };
         TokenPrinter.WriteResponse(tokenResponse, parameters);
         IApiCaller apiCaller = new ApiCaller();
@@ -227,24 +135,3 @@ public class Program
         await JwkGenerator.GenerateKey();
     }
 }
-
-internal static class StringExtensions
-{
-    public static string GetEmptyStringIfNotSet(this string? aString)
-    {
-        return aString ?? string.Empty;
-    }
-
-    public static List<string> GetListWithParameter(this string? aString)
-    {
-        return !string.IsNullOrEmpty(aString) ? new List<string>() { aString } : new List<string>();
-    }
-    
-    public static List<string> GetListWithMultipleParameters(this string? aString)
-    {
-        return string.IsNullOrEmpty(aString) ?
-            new List<string>() :
-            aString.Split(' ').ToList();
-    }
-}
-
