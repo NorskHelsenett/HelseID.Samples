6a5cd87c297d7526370ca49cf4e9a7ca251c2a1d
Magnus Bjerke Vik <magnus.bjerke.vik@conceptos.no>
2024-01-11 14:08:02 +0100

    Write jwks when generating key pair

---
+++ HelseId.RsaJwk/JsonWebKeySet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace HelseId.RsaJwk;
+
+internal class JsonWebKeySet
+{
+    public List<JsonWebKey> Keys { get; set; }
+}
