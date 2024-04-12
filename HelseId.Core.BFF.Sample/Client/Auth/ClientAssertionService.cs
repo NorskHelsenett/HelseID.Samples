992e41cf11865b9bcbf2d35fc42f410a65491847
Magnus Bjerke Vik <magnus.bjerke.vik@nhn.no>
2024-04-12 12:02:07 +0200

    [BFF] Fix ClientAssertionService not working with main branch of Duende.AccessTokenManagement

--- HelseId.Core.BFF.Sample/Client/Auth/ClientAssertionService.cs
+++ HelseId.Core.BFF.Sample/Client/Auth/ClientAssertionService.cs
@@ -7,16 +7,16 @@ namespace HelseId.Core.BFF.Sample.Client.Auth;
 
 public class ClientAssertionService : IClientAssertionService
 {
-    private readonly IOptionsSnapshot<HelseIdAuthOptions> _options;
+    private readonly IOptionsMonitor<HelseIdAuthOptions> _options;
 
-    public ClientAssertionService(IOptionsSnapshot<HelseIdAuthOptions> options)
+    public ClientAssertionService(IOptionsMonitor<HelseIdAuthOptions> options)
     {
         _options = options;
     }
 
     public Task<ClientAssertion?> GetClientAssertionAsync(string? clientName = null, TokenRequestParameters? parameters = null)
     {
-        var options = _options.Value;
+        var options = _options.CurrentValue;
 
         var clientAssertion = ClientAssertionBuilder.GetClientAssertion(options.ClientId, options.ClientJwk, options.Authority);
 
