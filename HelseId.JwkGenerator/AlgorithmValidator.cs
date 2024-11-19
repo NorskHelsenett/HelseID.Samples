57a6f4fef99acbfedc52bcdf05288228b52e20a6
Kåre <kare.brekke@nhn.no>
2024-11-19 14:16:38 +0100

    ﻿Defaults to EC, validates alg, logs more

---
+++ HelseId.JwkGenerator/AlgorithmValidator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+
+namespace HelseId.JwkGenerator;
+
+internal static class AlgorithmValidator
+{
+    private static HashSet<string> _helseIdApprovedRsaAlgorithms = new HashSet<string>(
+    [
+        SecurityAlgorithms.RsaSha256,
+        SecurityAlgorithms.RsaSha384,
+        SecurityAlgorithms.RsaSha512,
+        SecurityAlgorithms.RsaSsaPssSha256,
+        SecurityAlgorithms.RsaSsaPssSha384,
+        SecurityAlgorithms.RsaSsaPssSha512,
+    ]);
+
+    private static HashSet<string> _helseIdApprovedEcAlgorithms = new HashSet<string>(
+    [
+        SecurityAlgorithms.EcdsaSha256,
+        SecurityAlgorithms.EcdsaSha384,
+        SecurityAlgorithms.EcdsaSha512,
+    ]);
+
+    public static bool IsValidAlgorithm(string algorithm, KeyType keyType) =>
+        keyType == KeyType.Rsa
+        ? _helseIdApprovedRsaAlgorithms.Contains(algorithm)
+        : _helseIdApprovedEcAlgorithms.Contains(algorithm);
+}
---
+++ HelseId.JwkGenerator/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelseId.JwkGenerator;
+
+internal static class Logger
+{
+    public static void Info(string message)
+    {
+        LogWithColoredPrefix("Information: ", ConsoleColor.Blue, message);
+    }
+
+    public static void Warning(string message)
+    {
+        LogWithColoredPrefix("Warning: ", ConsoleColor.Yellow, message);
+    }
+
+    public static void Error(string message)
+    {
+        LogWithColoredPrefix("Error: ", ConsoleColor.Red, message);
+    }
+
+    public static void Success(string message)
+    {
+        LogWithColoredPrefix("Success: ", ConsoleColor.Green, message);
+    }
+
+    private static void LogWithColoredPrefix(string prefix, ConsoleColor color, string message)
+    {
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.Write(prefix);
+        Console.ForegroundColor = originalColor;
+        Console.WriteLine(message);
+    }
+}
