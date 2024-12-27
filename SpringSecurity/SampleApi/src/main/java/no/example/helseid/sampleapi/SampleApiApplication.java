d41d337179dac0906ce0701ff8fee04dde1ee6b4
oyvind-bech <95357172+oyvind-bech@users.noreply.github.com>
2024-12-27 09:33:09 +0100

    TestAPI for Spring Security  (#105)
    * Gitignore
    
    * Spring Security
    
    * New scopes

---
+++ SpringSecurity/SampleApi/src/main/java/no/example/helseid/sampleapi/SampleApiApplication.java
@@ -0,0 +1,12 @@
+package no.example.helseid.sampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApiApplication.class, args);
+    }
+}
